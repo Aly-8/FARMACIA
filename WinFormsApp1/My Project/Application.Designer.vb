78a9e1e58087eb973059bf94b5a414fd4dbfccd2
alyssallanto <alyssazchessca@gmail.com>
2025-05-05 23:39:10 +0800

    CHANGES 5/5/2025 11:39 PM

--- WinFormsApp1/My Project/Application.Designer.vb
+++ WinFormsApp1/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = New Stock()
+            Me.MainForm = New Cashier()
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
